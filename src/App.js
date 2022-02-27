fadabb95383855fabecdc50a0548046d3d8b74ec
Asher Azriel Ginting <asherginting3@gmail.com>
2022-02-27 10:35:54 +0700

    feat: init project file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button type="button" className='btn btn-primary'>Testing</Button>
+      <h1>Hello Bootstrap</h1>
     </div>
   );
 }
